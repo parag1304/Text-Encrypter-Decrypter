fabfbd7ca56080dd3c320b6b18cbe1b25f94902b
parag1304 <71979018+parag1304@users.noreply.github.com>
2021-08-22 22:57:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ while(True):
     print("HI, Do you want to encrpyt(e) or decrypt(d) or exit(ex) ")
     choice = input()
     if choice == "e" :
-        encrpyt()
+        encrypter.encrpyt()
     elif choice == "d" :
-        decrypt()
+        decrypter.decrypt()
     elif choice == "ex" :
         exit()
     else :
